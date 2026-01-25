158fb6d1c3ef54ef85433e085cd562e894629175
Lance Kent <lancemagollado46@gmail.com>
2026-01-25 20:08:48 +0800

    Studying freeCodeXCamp

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const person = {
     name: "Lance",
     age:22,
-    status:"single"
+    status:"single",
+    contact:{
+        phone:"09274330304",
+        email:{
+            personalEmail:"lancemagollado46@gmail.com",
+            schoolEmail:"lancemagollado@gmail.com"
+        }
+    }
 }
-let name = "name";
 
-console.log(person.hasOwnProperty(name));
+console.log(person.contact.email.personalEmail);
